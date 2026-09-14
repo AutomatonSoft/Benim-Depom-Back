afea40af6d4782ecbd1e3f19291b1fb38ce51f7f
Alex0515-ui <bulkairalihan@gmail.com>
2026-09-14 14:47:32 +0500

    Fix ruff

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,11 +7,11 @@ from .views import (
     LoginView,
     LogoutView,
     MeView,
-    PreferredLanguageView,
     PasswordChangeView,
     PasswordResetCompleteView,
     PasswordResetRequestView,
     PasswordResetVerifyView,
+    PreferredLanguageView,
     RegisterView,
 )
 
